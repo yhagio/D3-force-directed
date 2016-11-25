253fe30349877287f0e481ba03ef7d0e37647c86
Yuichi Hagio <yhagio87@gmail.com>
2016-11-24 21:51:11 -0500

    Updated

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ### Force Directed Relationship Map using D3
 
-You can see the relationship between campers(people) and links(hostname)
-that are submitted by them on FreeCodeCamp News
 
 # [DEMO](http://yhagio.github.io/D3-force-directed)
 
@@ -14,11 +12,3 @@ that are submitted by them on FreeCodeCamp News
 - [Simple Grph of Node & Link D3](http://stackoverflow.com/questions/28102089/simple-graph-of-nodes-and-links-without-using-force-layout)
 - [Basic Directional Force Layout Diagram](http://bl.ocks.org/d3noob/5141278)
 - [Sticky Force Layout](https://bl.ocks.org/mbostock/3750558)
-
-
-#### Steps
-- 1 Get data and format the data for links & nodes
-- 2 Create graph (SVG, Links, Nodes)
-- 3 Tooltips
-- 4 Show author picture on nodes
-- 5 Coloring
--- main.js
+++ main.js
@@ -1,76 +1,51 @@
 // Set up
-var width = 900;
-var height = 800;
+var dataSource = 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json';
+
+var width = 800;
+var height = 600;
 
 // Draw SVG
-var svg = d3.select("body").append("svg")
+var body = d3.select(".graph")
+var svg = body
+  .append("svg")
   .attr("width", width)
   .attr("height", height)
-  .style('background', 'linear-gradient(skyblue, white)');
+  .style('background', 'linear-gradient(#efefef, #fff)');
 
-var nodes = {};
-var links = [];
-
-// Get submitted link's hostname only
-function getHostname(url) {
-  var urlElement = document.createElement('a');
-  urlElement.href = url;
-  return urlElement.hostname;
-}
-
-d3.json('http://www.freecodecamp.com/news/hot', function(data) {
-  // Create links array 
-  data.forEach(function(d) {
-    var link = {
-      source: d.author.username,
-      target: getHostname(d.link),
-      rank: d.rank,
-      image: d.author.picture,
-      upvotes: d.upVotes.length
-    };
-    links.push(link);
-  });
-  
-  // Create nodes
-  links.forEach(function(link) {
-    // if (!link.image) { console.log(link.source, link.image);}
-
-    link.source = nodes[link.source] ||
-      (nodes[link.source] = {
-        nodeText: link.source, // text(name/url) for tooltip
-        rank: link.rank,
-        upvotes: link.upvotes,
-        image: link.image
-      });
-    link.target = nodes[link.target] ||
-      (nodes[link.target] = {
-        nodeText: link.target // text(name/url) for tooltip
-      });
-  });
+d3.json(dataSource, function(data) {
   
   // Set up force layout
   var force = d3.layout.force()
-      .nodes(d3.values(nodes))
-      .links(links)
+      .nodes(data.nodes)
+      .links(data.links)
       .size([width, height])
-      .linkDistance(50)
-      .charge(-130)
+      .linkDistance(50) // distance between connected nodes
+      .charge(-50) // acts like electrical charge on the nodes
       .on("tick", tick)
       .start();
   
-  // Append Link
+  // Append links
   var link = svg.selectAll(".link")
-    .data(force.links())
-    .enter().append("line")
+    .data(data.links)
+    .enter()
+    .append("line")
     .attr("class", "link");
   
-  // Append Node
-  var node = svg.selectAll(".node")
-    .data(force.nodes())
-    .enter().append("g")
-    .attr("class", "node")
+  // Append nodes (flags)
+  var node = body.select('.flags').selectAll(".node")
+    .data(data.nodes)
+    .enter()
+		.append('img')
+		.attr('class', function(d) { 
+      return 'flag flag-' + d.code })
+    .attr({
+      x: -5,
+      y: -5,
+      "width": 32,
+      "height": 32,
+    })
 
-    /********** TOOLTIP **********/
+    // Tooltip
     .on('mouseover', function(d) {
       // Show Tooltip Animation
       d3.select('#tooltip')
@@ -81,10 +56,10 @@ d3.json('http://www.freecodecamp.com/news/hot', function(data) {
       
       // Tooltip
       d3.select('#tooltip')
-        .style("left", d.x + 55 + "px")
-        .style("top", d.y + 35 + "px")
+        .style("left", (d.x + 1) + "px")
+        .style("top", d.y + 30 + "px")
         .select("#value")
-          .html('<strong class="nodeText">'+d.nodeText+'</strong>');
+          .html('<strong class="nodeText">' + d.country + '</strong>');
     })
     .on('mouseout', function(d) {
       // Hide Tooltip Animation
@@ -94,47 +69,19 @@ d3.json('http://www.freecodecamp.com/news/hot', function(data) {
         .duration(100)
         .style("opacity", 0);
     })
-    // Make the graph draggable
+    // Make the d3 draggable
     .call(force.drag);
   
-  // Size of circle representing the number 
-  // of connections (weight of the node)
-  node.append("circle")
-    .attr("r", function(d) {
-      if (d.weight === 1) {
-        return 4;
-      }
-      if (d.weight === 2) {
-        return 5
-      }
-      return d.weight * 2;
-    })
-    .attr('fill', '#FF99FF');
-  
-  // Append author picture
-  node
-    .append('image')
-    .attr("xlink:href", function(d){
-      return d.image;
-    })
-    .attr({
-      x: -15,
-      y: -15,
-      "width": 32,
-      "height": 32,
-    });
-
   // Define corrdinates of links and nodes
   function tick() {
+    node
+      .style('left', function(d) { return (d.x - 5) + "px" }) 
+			.style('top', function(d) { return (d.y - 5) + "px" });
+
     link
       .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
       .attr("y2", function(d) { return d.target.y; });
-
-    node
-      .attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")"; 
-      });
   }
 });
